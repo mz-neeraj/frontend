6011bfc817b0bc67acf4e419664590e1f9808641
Neeraj Sharma <murly1980@gmail.com>
2024-08-04 16:52:25 +0530

    Added user can be seen on console

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './User.css';
 const User = ()=>{
+    const[name,setName]=useState('');
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+    function addUser(){
+        console.log(name,email,password);
+    }
     return(
         <div className='user'>
             <h1>Add User</h1>
-            <input type='text' placeholder='Enter Name'></input>
-            <input type='text' placeholder='Enter Email'></input>
-            <input type='password' placeholder='Enter Password'></input>
-            <button>Add User</button>
+            <input  type='text' onChange={(e)=>{setName(e.target.value)}} placeholder='Enter Name'></input>
+            <input type='text' onChange={(e)=>{setEmail(e.target.value)}} placeholder='Enter Email'></input>
+            <input type='password' onChange={(e)=>{setPassword(e.target.value)}} placeholder='Enter Password'></input>
+            <button onClick={addUser}>Add User</button>
         </div>
     );
 }
